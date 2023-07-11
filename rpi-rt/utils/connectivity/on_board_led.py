5b500d2baa9b8388153113a7dc7e49e697836cfe
andre.liang <namwoam@gmail.com>
2023-07-11 06:24:50 +0100

    minor fix

--- rpi-rt/utils/connectivity/on_board_led.py
+++ rpi-rt/utils/connectivity/on_board_led.py
@@ -3,19 +3,22 @@ from time import sleep
 
 # Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
 # connector pin number, and the LED GPIO isn't on the connector
+
+pin = 21
+
 GPIO.setmode(GPIO.BCM)
 
 # set up GPIO output channel
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(pin, GPIO.OUT)
 while True:
     print("LED on")
     # On
-    GPIO.output(16, GPIO.LOW)
+    GPIO.output(pin, GPIO.LOW)
 
     # Wait a bit
     sleep(0.5)
     print("LED off")
     # Off
-    GPIO.output(16, GPIO.HIGH)
+    GPIO.output(pin, GPIO.HIGH)
 
     sleep(0.5)
