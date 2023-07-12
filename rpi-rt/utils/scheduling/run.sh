41a704d365a2f6243ab89c49d6d908fd08565c8f
andre.liang <namwoam@gmail.com>
2023-07-12 03:02:05 +0100

    old script will cause process not exiting

--- rpi-rt/utils/scheduling/run.sh
+++ rpi-rt/utils/scheduling/run.sh
@@ -1,4 +1,4 @@
 echo "running io critical test"
-timeout -k 10s 10s sudo ./io_critical
+sudo ./io_critical
 echo "running computation critical test"
-timeout -k 10s 10s sudo ./compute_critical
+sudo ./compute_critical
