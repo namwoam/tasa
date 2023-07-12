2dd3fe99a5b1d6c8c7ae8665d93ca055f4258a0b
andre.liang <namwoam@gmail.com>
2023-07-12 01:16:35 +0100

    testing scheduler

--- rpi-rt/utils/scheduling/main.c
+++ rpi-rt/utils/scheduling/main.c
@@ -30,17 +30,22 @@ int fib(int n)
 
 void *io_thread_func(void *data)
 {
+    struct timespec req, rem;
+    req.tv_nsec = 0; // 2000ms
+    req.tv_sec = 2;
     while (1)
     {
         time_t rawtime;
         struct tm *timeinfo;
         time(&rawtime);
         timeinfo = localtime(&rawtime);
-        fflush(stdout);
-        fprintf(stderr, "Hello at %s\n", asctime(timeinfo));
-        setbuf(stdout, NULL);
+        printf("Hello at %s\n", asctime(timeinfo));
         int t = rand() % 10;
-        usleep(1000 * (t + 2) * 100);
+        if (nanosleep(&req, &rem) == -1)
+        {
+            printf("error at nanosleep!\n");
+            exit(1);
+        }
     }
     return NULL;
 }
@@ -53,21 +58,21 @@ void *compute_thread_func(void *data)
         for (int i = 0; i < 20; i++)
         {
             result = fib(i);
-            fflush(stdout);
-            fprintf(stderr, "fib(%d)=%d\n",i ,result);
-            setbuf(stdout, NULL);
+            printf("fib(%d)=%d\n", i, result);
         }
     }
     return NULL;
 }
 
-int main(int argc, char *argv[])
+
+    int
+    main(int argc, char *argv[])
 {
     // initialize random number generator
     srand(time(NULL));
-    struct sched_param param;
-    pthread_attr_t norml_attr;
-    pthread_t thread;
+    struct sched_param critical_param, param;
+    pthread_attr_t compute_attr, io_attr;
+    pthread_t compute_thread, io_thread;
     int ret;
 
     /* Lock memory */
@@ -78,49 +83,91 @@ int main(int argc, char *argv[])
     }
 
     /* Initialize pthread attributes (default values) */
-    ret = pthread_attr_init(&attr);
+    ret = pthread_attr_init(&compute_attr);
+    if (ret)
+    {
+        printf("init pthread attributes failed\n");
+        goto out;
+    }
+    ret = pthread_attr_init(&io_attr);
     if (ret)
     {
         printf("init pthread attributes failed\n");
         goto out;
     }
     /* Set a specific stack size  */
-    ret = pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
+    ret = pthread_attr_setstacksize(&compute_attr, PTHREAD_STACK_MIN);
+    if (ret)
+    {
+        printf("pthread setstacksize failed\n");
+        goto out;
+    }
+    ret = pthread_attr_setstacksize(&io_attr, PTHREAD_STACK_MIN);
     if (ret)
     {
         printf("pthread setstacksize failed\n");
         goto out;
     }
     /* Set scheduler policy and priority of pthread */
-    ret = pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    ret = pthread_attr_setschedpolicy(&compute_attr, SCHED_FIFO);
     if (ret)
     {
         printf("pthread setschedpolicy failed\n");
         goto out;
     }
-    param.sched_priority = 80;
-    ret = pthread_attr_setschedparam(&attr, &param);
+    ret = pthread_attr_setschedpolicy(&io_attr, SCHED_FIFO);
+    if (ret)
+    {
+        printf("pthread setschedpolicy failed\n");
+        goto out;
+    }
+    critical_param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    param.sched_priority = sched_get_priority_min(SCHED_FIFO);
+    ret = pthread_attr_setschedparam(&compute_attr, &critical_param);
+    if (ret)
+    {
+        printf("pthread setschedparam failed\n");
+        goto out;
+    }
+    ret = pthread_attr_setschedparam(&io_attr, &param);
     if (ret)
     {
         printf("pthread setschedparam failed\n");
         goto out;
     }
     /* Use scheduling parameters of attr */
-    ret = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    ret = pthread_attr_setinheritsched(&compute_attr, PTHREAD_EXPLICIT_SCHED);
+    if (ret)
+    {
+        printf("pthread setinheritsched failed\n");
+        goto out;
+    }
+    ret = pthread_attr_setinheritsched(&io_attr, PTHREAD_EXPLICIT_SCHED);
     if (ret)
     {
         printf("pthread setinheritsched failed\n");
         goto out;
     }
     /* Create a pthread with specified attributes */
-    ret = pthread_create(&thread, &attr, compute_thread_func, NULL);
+    ret = pthread_create(&compute_thread, &compute_attr, compute_thread_func, NULL);
     if (ret)
     {
-        printf("create pthread failed\n");
+        printf("error code:%d\n", ret);
+        printf("create compute pthread failed\n");
+        goto out;
+    }
+    ret = pthread_create(&io_thread, &io_attr, io_thread_func, NULL);
+    if (ret)
+    {
+        printf("error code:%d\n", ret);
+        printf("create io pthread failed\n");
         goto out;
     }
     /* Join the thread and wait until it is done */
-    ret = pthread_join(thread, NULL);
+    ret = pthread_join(compute_thread, NULL);
+    if (ret)
+        printf("join pthread failed: %m\n");
+    ret = pthread_join(io_thread, NULL);
     if (ret)
         printf("join pthread failed: %m\n");
 
