cd4a3a973ccf7dc8d363cca7b54b4cc4738786e6
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-08-23 11:12:38 +0800

    fix graph

--- rpi-rt/utils/latency/analyze.py
+++ rpi-rt/utils/latency/analyze.py
@@ -2,7 +2,6 @@ import pandas as pd
 import os
 import re
 import matplotlib.pyplot as plt
-import subprocess
 
 record = []
 
@@ -19,6 +18,6 @@ with open(os.path.join(os.path.dirname(__file__), f"{target}.txt")) as reader:
             pass
 s = pd.Series(record)
 df = pd.DataFrame({"latency": s})
-df["latency"].plot(kind="hist",bins = 120, logy=True , xlabel="latency (us)" , title=f"hist-{target}").get_figure().savefig(
+df["latency"].plot(kind="hist", range= [0 , 12000] , bins = 120, logy=True , xlabel="latency (us)" , title=f"hist-{target} mean={df['latency'].mean()} us").get_figure().savefig(
     os.path.join(os.path.dirname(__file__), f"hist-{target}.png")
 )
